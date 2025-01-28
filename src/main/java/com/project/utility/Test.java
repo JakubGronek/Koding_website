8722e1aa3a825f6be903f5c453dc1c5a196206df
Maciej <fajerowsky.yt@gmail.com>
2025-01-28 19:59:09 +0100

    add: docker

--- src/main/java/com/project/utility/Test.java
+++ src/main/java/com/project/utility/Test.java
@@ -13,7 +13,7 @@ public class Test extends Thread {
     }
 
     public void test() {
-        ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c"
+        ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c",
                 "docker run --rm -v "+System.getProperty("user.dir")+"/temp/"+script_name+":/root/app/script.py python:latest python /root/app/script.py");
 
         try {
